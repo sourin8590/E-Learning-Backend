95a5f339d0da9ef81b2d89883911934c1f49302c
sourin8590 <souringhanty.20@gmail.com>
2024-09-07 02:13:44 +0530

    FrontEnd URL added

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://elearning-platform-plum.vercel.app",
     credentials: true,
     maxAge: 14400,
   })
